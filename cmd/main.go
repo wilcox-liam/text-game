6c9b2168677b4e38282353dd4c4f7af0e2f4bea0
wilcox-liam <wilcox.liam@gmail.com>
2020-03-22 13:35:01 -0300

    testing

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,13 @@ import (
  	"bufio"
  	"os"
  	"strings"
- 	"github.com/wilcox-liam/text-game"
+ 	"github.com/wilcox-liam/text-game/pkg" 
 )
 
 const game_name = "My Game"
 
 func main() {
-	var current_room *Room
+	var current_room *text_game.Room
 
 	current_room = set_initial_state()
 
@@ -33,8 +33,8 @@ func main() {
 }
 
 //Updates the Game State
-func update_state(current_room *Room, input string) *Room {
-	var next_room *Room
+func update_state(current_room *text_game.Room, input string) *text_game.Room {
+	var next_room *text_game.Room
 
 	if input == "n" {
 		next_room = current_room.go_north()
@@ -57,11 +57,11 @@ func update_state(current_room *Room, input string) *Room {
 }
 
 //Sets the initial game state
-func set_initial_state() *Room {
-	room1:= Room{"Room 1", "Welcome to room x", nil, nil, nil, nil, nil}
-	room2 := Room{"Room 2", "Welcome to room 2", nil, nil, nil, nil, nil}
-	room3 := Room{"Room 3", "Welcome to room 3", nil, nil, nil, nil, nil}
-	room4 := Room{"Room 4", "Welcome to room 4", nil, nil, nil, nil, nil}
+func set_initial_state() *text_game.Room {
+	room1:= text_game.Room{"Room 1", "Welcome to room x", nil, nil, nil, nil}
+	room2 := text_game.Room{"Room 2", "Welcome to room 2", nil, nil, nil, nil}
+	room3 := text_game.Room{"Room 3", "Welcome to room 3", nil, nil, nil, nil}
+	room4 := text_game.Room{"Room 4", "Welcome to room 4", nil, nil, nil, nil}
 
 	room1.North = &room2
 	room2.East = &room3
--- pkg/game.go
+++ pkg/game.go
@@ -1,4 +1,4 @@
-package text-game
+package text_game
 
 type Room struct  {
 	Name string
@@ -7,33 +7,27 @@ type Room struct  {
 	East *Room
 	South *Room
 	West *Room
-	Items []*Item
 }
 
-type Item struct  {
-	Name string
-}
-
-func (r Room) go_north() *Room {
+func (r Room) Go_north() *Room {
 	return r.North
 }
 
-func (r Room) go_east() *Room {
+func (r Room) Go_east() *Room {
 	return r.East
 }
 
-func (r Room) go_south() *Room {
+func (r Room) Go_south() *Room {
 	return r.South
 }
 
-func (r Room) go_west() *Room {
+func (r Room) Go_west() *Room {
 	return r.West
 }
 
-func (r Room) get_options() string {
+func (r Room) Get_options() string {
 	options := ""
 	if r.North != nil {
-
 		options += " [North] "
 	}
 	if r.East != nil {
